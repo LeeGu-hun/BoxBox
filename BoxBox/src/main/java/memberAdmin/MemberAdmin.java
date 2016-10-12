f54a8983361f81eda1af63687b8e5bcb2287614b
Chanyoon Lee <Chanyoon Lee@ChanyoonLee-PC>
2016-10-13 08:42:52 +0900

    a

---
+++ BoxBox/src/main/java/memberAdmin/MemberAdmin.java
@@ -0,0 +1,75 @@
+package memberAdmin;
+
+public class MemberAdmin {
+	private String id;
+	private String phone;
+	private int type, amount;	
+	private String email, name, password;
+	
+	public MemberAdmin(String id, String phone, int type, int amount, String email, String name, String password) {
+		super();
+		this.id = id;
+		this.phone = phone;
+		this.type = type;
+		this.amount = amount;
+		this.email = email;
+		this.name = name;
+		this.password = password;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+}
